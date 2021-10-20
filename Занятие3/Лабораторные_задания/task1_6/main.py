11d9c888552ece6d01419c336d2d4f7c65a2f9b7
n1kolayvas1lev <ckb01@ya.ru>
2021-10-20 21:20:58 +0300

    Классная работа 20.10.21

--- Занятие3/Лабораторные_задания/task1_6/main.py
+++ Занятие3/Лабораторные_задания/task1_6/main.py
@@ -1,7 +1,7 @@
 def f(a_, b_):
     sum_ = 0
     for i in range(1, L_YEAR+1):
-        sum_ += b_ * i * INFLATION
+        sum_ += b_ * i ** INFLATION
     return sum_ - a_ * L_YEAR
 if __name__ == "__main__":
     a = int(input())
